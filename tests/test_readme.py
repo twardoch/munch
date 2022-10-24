d9cfeac3d770be37f49dcf4a53379d08a7d28acf
David Sternlicht <d1618033@gmail.com>
2022-10-24 10:41:01 +0300

    Fix CI (#89)
    * add upgrade pip and build tools
    
    * fix test readme
    
    * Install yaml too, remove skip older python versions
    
    * fix req
    
    * Install yaml for tests
    
    * restrict branches
    
    * remove unused import

--- tests/test_readme.py
+++ tests/test_readme.py
@@ -1,6 +1,5 @@
 import doctest
 import os
-import sys
 import pytest
 
 _HERE = os.path.abspath(os.path.dirname(__file__))
@@ -8,11 +7,10 @@ _README_PATH = os.path.join(_HERE, '..', 'README.md')
 assert os.path.exists(_README_PATH)
 
 
-@pytest.mark.skipif(sys.version_info[:2] < (3, 6), reason="Requires Python version >= 3.6")
 @pytest.mark.usefixtures("yaml")
 def test_readme():
     globs = {
-        'print_function': print_function,
+        'print_function': print
     }
     result = doctest.testfile(_README_PATH, module_relative=False, globs=globs)
     assert not result.failed
